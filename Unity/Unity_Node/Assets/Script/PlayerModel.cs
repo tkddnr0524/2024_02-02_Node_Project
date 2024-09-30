dd757414973b9ea0ac3c1635747875d8f33b61c9
tkddnr0524 <tkddnr0110@naver.com>
2024-09-30 15:46:07 +0900

    [Feat] GameAPI 스크립트 작성

---
+++ Unity/Unity_Node/Assets/Script/PlayerModel.cs
@@ -0,0 +1,49 @@
+using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+
+
+[Serializable]
+public class PlayerModel
+{
+    public string playerName;
+    public int metal;
+    public int crystal;
+    public int deuterium;
+    public List<PlanetModel> Planets;
+    public PlayerModel(string name)
+    {
+        this.playerName = name;
+        this.metal = 500;
+        this.crystal = 300;
+        this.deuterium = 100;
+    }
+
+    public void CollectResources()
+    {
+        metal += 10;
+        crystal += 5;
+        deuterium += 2;
+    }
+}
+
+[Serializable]
+
+public class PlanetModel
+{
+    public int id;
+    public string name;
+    public int metal;
+    public int crystal;
+    public int deuterium;
+
+    public PlanetModel(int id, string name)
+    {
+        this.id = id;
+        this.name = name;
+        this.metal = 500;
+        this.crystal = 300;
+        this.deuterium = 100;
+    }
+}
+
